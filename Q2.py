84b8e6212030599f9fcb956f28e97beea7c08641
AlineJPC <57627411+AlineJPC@users.noreply.github.com>
2019-11-20 14:32:40 -0300

    Corrigidos BUGS, adicionado chamado a função main

--- Q2.py
+++ Q2.py
@@ -5,10 +5,6 @@
 
 def inversao(mat):
     aux = 0
-    for linhas in range(2):
-        for colunas in range(2):
-            print(mat[linhas][colunas], end='\t')
-        print("\n")
     det = (mat[0][0]*mat[1][1]) - (mat[0][1]*mat[1][0])
     if det==0:
         return None
@@ -27,9 +23,18 @@ def main():
         matriz[linhas]=[0]*2
     for linhas in range(2):
         for colunas in range(2):
-            matriz[linhas][colunas] = float(input("Insira o elemento:"))
-    matriz = inversao(matriz)
+            matriz[linhas][colunas] = float(input("Insira o elemento [" + str(linhas) + "][" + str(colunas) + "]: "))
+    print("\n")
+    print("Matriz original: ")
     for linhas in range(2):
         for colunas in range(2):
-            print(mat[linhas][colunas], end ='\t')
+            print(matriz[linhas][colunas], end='\t')
         print("\n")
+    mat = inversao(matriz)
+    print("Matriz inversa: ")
+    for linhas in range(2):
+        for colunas in range(2):
+            print(mat[linhas][colunas], end='\t')
+        print("\n")
+
+main()
