b228d0ed8fed02cbc94c80f220ad88078e0282f8
AlineJPC <57627411+AlineJPC@users.noreply.github.com>
2019-11-20 15:38:42 -0300

    Funcoes corrigidas e adiciconadas
    Funções das letras a) e b) com as correções necessárias, funções das letras c) e d) adicionadas e feita parte da função main

--- Q1.py
+++ Q1.py
@@ -1,7 +1,7 @@
 # Autor: Aline Capucho
 # Data: 19/11/19
 
-
+import os
 
 def func_data(data):
     ano = ""
@@ -22,9 +22,40 @@ def func_data(data):
 
 def func_atv(ativo):
     if len(ativo)>3:
-        ativo.del(3)
-    if(ativo)=="SIM":
+        ativo = ativo.replace('\n', '')
+    if ativo=="SIM":
         return True
     else:
         return False
 
+def func_list(texto):
+    aux = 1
+    for cont in range(len(texto)):
+        if texto[cont]==":":
+            aux+=1
+    lista = [""]*aux
+    aux = 0
+    for cont in range(len(texto)):
+        if texto[cont]!=":":
+            lista[aux]+= texto[cont]
+        else:        
+            aux+=1
+    return lista
+
+def func_ler(arq):
+    lista = []
+    for line in arq:
+        lista.append(line)
+    return lista
+        
+def main():
+    cwd = os.getcwd()
+    arq = open(cwd + "\cadastro.txt", "r")
+    arquivo = func_ler(arq)
+    lista = []
+    for cont in range(len(arquivo)):
+        lista.append(func_list(arquivo[cont]))
+    print(lista)    
+    
+main()
+    
